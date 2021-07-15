a4bcef9aebdcf7acc4d888d148b3d1bd188353b6
NelsonDaniel <nd.ta@hotmail.com>
2021-07-14 20:34:35 -0400

    fixed bug for unbind/bind devices

--- UsbIpServer/RegistryWatcher.cs
+++ UsbIpServer/RegistryWatcher.cs
@@ -28,7 +28,7 @@ namespace UsbIpServer
         {
             // something changed in the registry, so check if we should unbind device
             var connectedDevices = await ExportedDevice.GetAll(CancellationToken.None);
-            var devicesToUnbind = connectedDevices.Where(x => RegistryUtils.IsDeviceAvailable(x.BusId));
+            var devicesToUnbind = connectedDevices.Where(x => !RegistryUtils.IsDeviceAvailable(x.BusId));
             foreach (var device in devicesToUnbind)
             {
                 if (devices.ContainsKey(device.BusId))
