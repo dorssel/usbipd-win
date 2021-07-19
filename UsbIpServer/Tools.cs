f734a517f927b889ab98387b8f0c35495c72edca
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2021-07-19 18:29:09 +0200

    fix build
    Split off CsWin32 to separate code generating project to avoid some
    (pre-release) bugs like including Docs in output and assembly version
    mismatches.

--- UsbIpServer/Tools.cs
+++ UsbIpServer/Tools.cs
@@ -8,7 +8,6 @@ using System.ComponentModel;
 using System.Globalization;
 using System.IO;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text.RegularExpressions;
 using System.Threading;
@@ -35,7 +34,13 @@ namespace Windows.Win32.System.SystemServices
         /// </summary>
         public static ref DEVPROPKEY From(in PROPERTYKEY propertyKey)
         {
-            return ref Unsafe.As<PROPERTYKEY, DEVPROPKEY>(ref Unsafe.AsRef(in propertyKey));
+            unsafe
+            {
+                fixed (PROPERTYKEY* pk = &propertyKey)
+                {
+                    return ref *(DEVPROPKEY*)pk;
+                }
+            }
         }
     }
 }
