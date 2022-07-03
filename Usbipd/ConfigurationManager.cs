ac36ff2eb819fa552982e38cda5a6de16dcda16c
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2022-07-03 12:46:46 +0200

    Disable spell checker
    - too spammy

--- Usbipd/ConfigurationManager.cs
+++ Usbipd/ConfigurationManager.cs
@@ -192,7 +192,7 @@ static class ConfigurationManager
 
         try
         {
-            // The FriendlyName (if it exists) is usefull for USB\COMPOSITE, but the Description is not.
+            // The FriendlyName (if it exists) is useful for USB\COMPOSITE, but the Description is not.
             // So, if FriendlyName exists, we start the list with that.
             var friendlyName = ((string)Get_DevNode_Property(deviceNode, PInvoke.DEVPKEY_Device_FriendlyName)).Trim();
             if (!string.IsNullOrEmpty(friendlyName))
