bcb9ac4710baf27094b77b6892e3a74a91e08069
Frans van Dorsselaer <17404029+dorssel@users.noreply.github.com>
2026-04-25 18:40:32 +0200

    Finish auto update driver action

--- UnitTests/DriverDetails_Tests.cs
+++ UnitTests/DriverDetails_Tests.cs
@@ -35,6 +35,6 @@ sealed class DriverDetails_Tests
     public void Version()
     {
         // Keep this test up-to-date when updating the driver.
-        Assert.AreEqual(new Version(7, 2, 6, 22322), DriverDetails.Instance.Version);
+        Assert.AreEqual("7.2.6", DriverDetails.Instance.Version.ToString(3));
     }
 }
